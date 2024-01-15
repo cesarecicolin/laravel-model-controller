3a17921dd115877b68be4ccc6379352ac0d52959
cesarecicolin <cece.caster@gmail.com>
2024-01-15 14:59:53 +0100

    add: card con film e titolo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,32 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+<section class=" pt-5 pb-5 ">
+    <div class="container">
+        <div class="row  row-cols-6 ">
+            @foreach ($movies as $movie)
+                <div>
+                    <img class="my-img" src="{{ $movie->image }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+
+                    </div>
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+
+
+
+                </div>
+            @endforeach
+        </div>
+            <div class="text-center">
+                <button class="btn btn-primary">LOAD MORE</button>
+            </div>
+                
+
+
+    </div>
+
+
+
+
+</section>
 @endsection
